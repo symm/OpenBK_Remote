e2bdf280ee0831b19b807ec997c09cf0eaf23744
Gareth Jones <me@gazj.co.uk>
2023-08-16 23:28:32 +0100

    Button ordering

--- startup.js
+++ startup.js
@@ -37,21 +37,21 @@ function installHtmx() {
 function createRemote() {
     // Topping DX3 Pro+
     var topping = [
-        createButton("Mute", "NEC-88-6-0"),
         createButton("Power", "NEC-88-18-0"),
         createButton("Volume +", "NEC-88-46-0"),
         createButton("Volume -", "NEC-88-16-0"),
-        createButton(">", "NEC-88-15-0"),
+        createButton("Mute", "NEC-88-6-0"),
         createButton("<", "NEC-88-47-0"),
         createButton("Middle", "NEC-88-55-0"),
+        createButton(">", "NEC-88-15-0"),
         createButton("Headphones", "NEC-88-4-0"),
         createButton("Line out", "NEC-88-40-0"),
     ]
 
     var lgc1 = [
         createButton("Power", "NEC-4-8-0"),
-        createButton("Volume -", "NEC-4-3-0"),
         createButton("Volume +", "NEC-4-2-0"),
+        createButton("Volume -", "NEC-4-3-0"),
     ]
 
     var remotes = [
